c2bfc57674650836bd6fc1bfe0c164d56796e3a8
William Chen <chtsweiliang@gmail.com>
2021-08-11 21:29:25 +0800

    Anonymous class by ActionListener (swing)

---
+++ src/main/java/com/wei/ByeActionListener.java
@@ -0,0 +1,11 @@
+package com.wei;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ByeActionListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        System.out.println("Bye!");
+    }
+}
